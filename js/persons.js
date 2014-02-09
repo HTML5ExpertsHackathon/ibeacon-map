92a1c36225e930c738e40c203ff60830515ce717
Ryosuke Otsuya <r.otsuya@gmail.com>
2014-02-09 10:33:57 +0900

    画像サイズを修正。

--- js/persons.js
+++ js/persons.js
@@ -17,10 +17,8 @@
         person.x = position.x;
         person.y = position.y;
 
-        if (isKnownPersonId) {
-            person.scaleX = 0.5;
-            person.scaleY = 0.5;
-        }
+        person.scaleX = 0.5;
+        person.scaleY = 0.5;
 
         person.move = function(position) {
             createjs.Tween.get(this).to(position, 500);
